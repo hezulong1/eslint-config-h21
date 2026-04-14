6c3dbb38f6d36ce0735246dd90de94c21c9f6713
hezulong1 <h2108b@163.com>
2026-04-14 16:24:31 +0800

    add 2 test file

---
+++ fixtures/import-exports.ts
@@ -0,0 +1,8 @@
+/* eslint-disable  */
+
+
+export default class MyClass { /*...*/ } // Multiple default exports.
+
+function makeClass() { return new MyClass(...r) }
+
+export default makeClass // Multiple default exports.
---
+++ fixtures/import-x.ts
@@ -0,0 +1,12 @@
+// import foo from './if-newline_template-curly-spacing'
+
+// initFoo(a)
+
+// import bar from './bar'
+
+import foo from './h';
+
+const foo = await import('./h');
+
+// eslint-disable-next-line no-console
+console.log(foo);
